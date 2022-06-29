e5f114d644a297956ae9fcda12af3dfe60a1a79c
Rhaqim <anusiemj@gmail.com>
2022-06-29 14:05:55 +0100

    deleted global css styles and included fonts

--- defaultTheme.js
+++ defaultTheme.js
@@ -1,57 +1,71 @@
 import { extendTheme } from "@chakra-ui/react";
 import { mode } from '@chakra-ui/theme-tools'
 
-const theme = extendTheme({
-    colors: {
-        brand: {
-            900: "#1a365d",
-            100: "#5e99cc"
-        },
-        neutral: {
-            900: "#1a365d",
-            100: "#5e99cc"
-        },
-        success: {
-            900: "#1a365d",
-            100: "#5e99cc"
+const styles = {
+    global: props => ({
+        body: {
+            bg: mode('#f0e7db', '#202023')(props),
+            color: mode('#202023', '#f0e7db')(props),
+            fontFamily: 'Menlo, monospace'
         }
-    },
-    fonts: {
-        body: "Roboto, system-ui, sans-serif",
-        heading: "Roboto, system-ui, sans-serif",
-        monospace: "Menlo, monospace",
-    },
-    fontWeights: {
-        hairline: 100,
-        thin: 200,
-        light: 300,
-        normal: 400,
-        medium: 500,
-        semibold: 600,
-        bold: 700,
-        extrabold: 800,
-        black: 900,
-    },
-    components: {
-        Heading: {
-            variants: {
-                'section-title': {
-                    textDecoration: 'underline',
-                    fontSize: 20,
-                    textUnderlineOffset: 6,
-                    textDecorationColor: '#525252',
-                    textDecorationThickness: 4,
-                    marginTop: 3,
-                    marginBottom: 4
-                }
+    })
+}
+
+const components = {
+    Heading: {
+        variants: {
+            'section-title': {
+                textDecoration: 'underline',
+                fontSize: 20,
+                textUnderlineOffset: 6,
+                textDecorationColor: '#525252',
+                textDecorationThickness: 4,
+                marginTop: 3,
+                marginBottom: 4
             }
         }
     },
-    config: {
-        initialColorMode: 'dark',
-        useSystemColorMode: true
+    Link: {
+        baseStyle: props => ({
+            color: mode('#3d7aed', '#ff63c3')(props),
+            textUnderlineOffset: 3,
+            fontFamily: 'Menlo, monospace'
+        })
     }
-});
+}
 
-export default theme;
+const fonts = {
+    heading: "'M PLUS Rounded 1c'",
+    body: "Roboto, system-ui, sans-serif",
+    monospace: "Menlo, monospace",
+}
+
+const colors = {
+    grassTeal: '#88ccca',
+    brand: {
+        900: "#1a365d",
+        100: "#5e99cc"
+    }
+}
+
+const fontWeights = {
+    hairline: 100,
+    thin: 200,
+    light: 300,
+    normal: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+    extrabold: 800,
+    black: 900,
+}
+
+const config = {
+    initialColorMode: 'dark',
+    useSystemColorMode: true
+}
+
+const theme = extendTheme({ config, styles, components, fonts, colors, fontWeights })
+
+export default theme
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import defaultTheme from '../defaultTheme'
 import Banner from '../components/Banner'
+import Fonts from '../fonts'
 
 function MyApp({ Component, pageProps, router }) {
   return (
     <ChakraProvider theme={defaultTheme}>
+      <Fonts />
       <Banner router={router}>
         <Component {...pageProps} />
       </Banner>
