5c14d5deba40e3c1bf6910b4d6aa7f21202b9c95
Rhaqim <anusiemj@gmail.com>
2022-07-19 23:22:42 +0100

    creating base scene for all rendered objects, need no background image and also need to scale properly

---
+++ components/models/BaseScene.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+
+const BaseScene = () => {
+  return (
+    <Canvas>
+        
+    </Canvas>
+  )
+}
+
+export default BaseScene
