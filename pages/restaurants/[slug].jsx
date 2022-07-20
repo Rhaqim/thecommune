b4d9f6307414d65f1acc01cce0be7dae2b1e3611
Rhaqim <anusiemj@gmail.com>
2022-07-20 02:24:47 +0100

    used test data to check fetching and populating data along with different slugs

--- pages/restaurants/[slug].jsx
+++ pages/restaurants/[slug].jsx
@@ -1,12 +1,45 @@
-import React from 'react'
-import Hero from '../../components/Hero'
+import React from "react";
+import Hero from "../../components/Hero";
 
-const RestaurantsPage = () => {
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const json = await res.json();
+  return {
+    // paths: json.map(restaurant => ({
+    //   params: { slug: restaurant.name.toLowerCase().replace(/ /g, "-") },
+    // })),
+    paths: json.map(restaurant => ({
+      params: { slug: restaurant.id.toString() },
+    })),
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${params.slug}`
+  );
+  const json = await res.json();
+  return {
+    props: {
+      title: "Restaurants",
+      description: "Restaurants",
+      keywords: "Restaurants",
+      restaurant: json,
+    },
+  };
+};
+
+const RestaurantsPage = ({restaurant}) => {
   return (
     <div>
       <Hero heading="Restaurants" message="Slug Page" />
+      <h1>{restaurant.name}</h1>
+      <p>{restaurant.email}</p>
+      <p>{restaurant.phone}</p>
+      <p>{restaurant.website}</p>
     </div>
-  )
-}
+  );
+};
 
-export default RestaurantsPage
+export default RestaurantsPage;
--- pages/restaurants/index.jsx
+++ pages/restaurants/index.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import Hero from '../../components/Hero'
+import React from "react";
+import Link from "next/link";
+import Hero from "../../components/Hero";
 
-const restaurants = () => {
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const json = await res.json();
+
+  return {
+    props: {
+      title: "Restaurants",
+      description: "Restaurants",
+      keywords: "Restaurants",
+      restaurants: json,
+    },
+  };
+};
+
+const restaurants = ({ restaurants }) => {
   return (
     <div>
       <Hero heading="Restaurants" message="Restaurants Page" />
+      {restaurants.map(restaurant => (
+        <div key={restaurant.id}>
+          {/* <Link
+            href={`/restaurants/${restaurant.name
+              .toLowerCase()
+              .replace(/ /g, "-")}`}
+          > */}
+          <Link
+            href={`/restaurants/${restaurant.id}`}
+          >
+            {restaurant.name}
+          </Link>
+          <p>{restaurant.description}</p>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default restaurants
+export default restaurants;
