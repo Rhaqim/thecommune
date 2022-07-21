21681e972c1426aa517fed38ca01a20030f5a894
Rhaqim <anusiemj@gmail.com>
2022-07-21 10:05:02 +0100

    styling grid for the restaurants section to display base info

--- components/RestaurantsSection.jsx
+++ components/RestaurantsSection.jsx
@@ -6,11 +6,7 @@ const RestaurantsSection = ({ children }) => {
       <div />
       <div className="py-10 justify-center items-center  bg-opacity-10 rounded-xl m-2">
         <h1 className="py-10 text-5xl text-white font-bold">Restaurants</h1>
-        <div className="grid grid-rows-none md:grid-cols-5 p-4 md:gap-4">
-          <div className="w-full h-full">
-            {children}
-          </div>
-        </div>
+        <div className="w-full h-full">{children}</div>
       </div>
     </div>
   );
--- pages/restaurants/index.js
+++ pages/restaurants/index.js
@@ -21,16 +21,19 @@ const restaurants = ({ restaurants }) => {
   return (
     <RestaurantsSection>
       {restaurants.map(restaurant => (
-        <div key={restaurant.id} className="p-4">
-          {/* <Link
-          href={`/restaurants/${restaurant.name
-            .toLowerCase()
-              .replace(/ /g, "-")}`}
-          > */}
+        <div key={restaurant.id} className="border border-gray-400 border-spacing-2 rounded-lg m-4">
           <Link href={`/restaurants/${restaurant.id}`}>
-            {restaurant.name}
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-8 gap-2 p-4 cursor-pointer items-center">
+              <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt={restaurant.name} />
+              <p>{restaurant.name}</p>
+              <p>{restaurant.website}</p>
+              <p>{restaurant.email}</p>
+              <p>{restaurant.address.street}</p>
+              <p>{restaurant.address.city}</p>
+              <p>{restaurant.address.zipcode}</p>
+              <p>{restaurant.phone}</p>
+            </div>
           </Link>
-          <p>{restaurant.website}</p>
         </div>
       ))}
     </RestaurantsSection>
