189ed71ff77e240a3d173f784c98759a374c53e1
Rhaqim <anusiemj@gmail.com>
2022-07-24 19:11:48 +0100

    changed the font-family to Poppins and Regular 400

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,10 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100&display=swap" rel="stylesheet" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Roboto+Mono:wght@100&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
