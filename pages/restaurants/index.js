a479726096fe50e0f9e03ccb218f520511ef4a13
Rhaqim <anusiemj@gmail.com>
2022-08-03 15:55:04 +0100

    changed the mobile row to 2 items per row

--- pages/restaurants/index.js
+++ pages/restaurants/index.js
@@ -32,7 +32,7 @@ const restaurants = ({ restaurants, photos }) => {
   return (
     <RestaurantsSection>
       <div className="max-w-2xl mx-auto px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-5 xl:gap-x-6">
+        <div className="grid grid-cols-2 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-5 xl:gap-x-6">
           {photos.map((photo) => (
             <Link key={photo.id} href={`/restaurants/${photo.id}`}>
               <motion.div
