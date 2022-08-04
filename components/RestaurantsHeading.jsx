0997b8ed1b901c472d69f853bfa2af0f25d834ad
Rhaqim <anusiemj@gmail.com>
2022-08-04 22:30:20 +0100

    adjusted the searchbar in mobile and also made restuarant text center when in mobile

--- components/RestaurantsHeading.jsx
+++ components/RestaurantsHeading.jsx
@@ -3,11 +3,10 @@ import SearchBar from "./SearchBar";
 
 const RestaurantsHeading = ({ children }) => {
   return (
-    <div className="max-w-[1240] mx-auto p-2">
-      <div />
-      <div className="py-10 justify-center items-center">
-        <div className="lg:flex py-16 justify-between ">
-          <h1 className="text-5xl text-white font-bold">Restaurants</h1>
+    <div className="max-w-[1240] mx-2 p-2">
+      <div className="pt-10 justify-center items-center">
+        <div className="lg:flex pt-16 justify-between">
+          <h1 className="font-bold text-5xl text-center text-white">Restaurants</h1>
           <div className="search-bar">
             <SearchBar />
           </div>
