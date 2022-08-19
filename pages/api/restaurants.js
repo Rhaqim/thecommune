b090c17c9a61d10b841a432c7b69ca4e97d9b428
Rhaqim <anusiemj@gmail.com>
2022-08-19 22:32:56 +0100

    added ajv to validate post data for restaurants, removed console log from DBexecutions, removed comments from restaurants page

--- pages/api/restaurants.js
+++ pages/api/restaurants.js
@@ -1,4 +1,6 @@
 const { getRestaurants, creatNewRestaurant } = require('../../lib/DB/DBExecutions');
+const Ajv = require('ajv');
+const ajv = new Ajv();
 
 export default async function handler(req, res) {
     if (req.method === "GET") {
@@ -9,12 +11,50 @@ export default async function handler(req, res) {
             res.status(404).json({ message: "No restaurants found" });
         }
     } else if (req.method === "POST") {
-        const { review } = req.body;
-        const result = await creatNewRestaurant(review);
-        if (result) {
-            res.status(201).json({ message: "Restaurant added" });
-        } else {
-            res.status(404).json({ message: "Restaurant not added" });
+        // validate the request body first
+        const validate = ajv.compile(schema);
+        const valid = validate(req.body);
+        if (!valid) {
+            res.status(400).json({ message: "Invalid request body" + validate.errors });
+            return;
         }
+        // create new restaurant
+        const data = res.body
+        data.createdAt = new Date();
+        data.updatedAt = new Date();
+        // const result = await creatNewRestaurant(data);
+        // if (result) {
+        //     res.status(201).json({ message: "Restaurant created" });
+        // } else {
+        //     res.status(500).json({ message: "Something went wrong" });
+        // }
+
     }
 }
+
+// Validation Schema
+const schema = {
+    type: "object",
+    properties: {
+        title: {type : "string"},
+        description: {type : "string"},
+        slug: {type : "string"},
+        address: {type : "string"},
+        phone: {type : "string"},
+        email: {type : "string"},
+        website: {type : "string"},
+        images: {type: "array"},
+        latitude: {type : "string"},
+        longitude: {type : "string"},
+        rating: {type : "integer"},
+        openingTime: {type: "array"},
+        currency: {type : "string"},
+        avgPrice: {type : "integer"},
+        categories: {type: "array"},
+        tags: {type: "array"},
+        createdAt: {type: "string"},
+        updatedAt: {type: "string"},
+    },
+    required: ["title", "description", "slug", "address", "phone", "email", "website", "images", "latitude", "longitude", "rating", "openingTime", "currency", "avgPrice", "categories", "tags", "createdAt", "updatedAt"],
+    additionalProperties: false,
+};
--- pages/restaurants/index.js
+++ pages/restaurants/index.js
@@ -66,18 +66,3 @@ const restaurants = ({ restaurants, photos }) => {
 };
 
 export default restaurants;
-
-
-{/*
-      {restaurants.map(restaurant => (
-        <div key={restaurant.id} className="border border-gray-400 border-spacing-2 rounded-lg m-4">
-          <Link href={`/restaurants/${restaurant.id}`}>
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-[2rem] p-4 cursor-pointer items-center">
-              <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt={restaurant.name} />
-              <h1 className="font-bold text-2xl text-white">{restaurant.name}</h1>
-              <p>{restaurant.email}</p>
-            </div>
-          </Link>
-        </div>
-      ))}
-      */}
