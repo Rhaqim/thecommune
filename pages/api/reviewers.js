6384b44b6fe905a6b152c04398dd7c6bc98261d3
Rhaqim <anusiemj@gmail.com>
2022-08-23 11:50:06 +0100

    api to add users created

---
+++ pages/api/reviewers.js
@@ -0,0 +1,54 @@
+const { getReviewer, createUser } = require('../../lib/DB/DBExecutions');
+const Ajv = require('ajv');
+const ajv = new Ajv();
+
+export default async function handler(req, res) {
+    if (req.method === "GET") {
+        const { id } = req.query;
+        const result = await getReviewer(id);
+        if (result) {
+            res.status(200).json(result);
+        } else {
+            res.status(404).json({ message: "No Reveiwer with that ID found" });
+        }
+    } else if (req.method === "POST") {
+        // validate the request body first
+        const validate = ajv.compile(schema);
+        const valid = validate(req.body);
+        if (!valid) {
+            res.status(400).json({ message: "Invalid request body" + validate.errors });
+            return;
+        }
+        // change created_at to current date
+        req.body.role = "user";
+        req.body.createdAt = new Date();
+        req.body.updatedAt = new Date();
+
+        const result = await createUser(req.body);
+        if (result) {
+            res.status(201).json({ message: "Reviewer created" });
+        }
+        else {
+            res.status(500).json({ message: "Something went wrong" });
+        }
+
+    }
+}
+
+// Validation Schema
+const schema = {
+    type: "object",
+    properties: {
+        fullname: { type: "string" },
+        username: { type: "string" },
+        avatar: { type: "object" },
+        email: { type: "string" },
+        password: { type: "string" },
+        socials: { type: "object" },
+        role: { type: "string" },
+        createdAt: { type: "string" },
+        updatedAt: { type: "string" },
+    },
+    required: ["username", "email", "password", "createdAt", "updatedAt"],
+    additionalProperties: false,
+};
