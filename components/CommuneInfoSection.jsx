000dfa656cfeeb3c9e88485d5cd8611480697e9a
Rhaqim <anusiemj@gmail.com>
2022-08-23 22:16:57 +0100

    Added the restaurant avg rating  and budget

--- components/CommuneInfoSection.jsx
+++ components/CommuneInfoSection.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-const CommuneInfoSection = () => {
+const CommuneInfoSection = ({ avgRating, budget, meal_of_the_week }) => {
   return (
     <div className="bg-gray-600">
       <div className="py-5">
         <ul className="text-white text-center grid lg:grid-cols-2 grid-cols-2 mid-content">
           <li className="p-4">
-            <h1 className="sm:text-sm lg:text-4xl md:text-3xl font-bold p-2">Avg Rating</h1>
+            <h1 className="sm:text-sm lg:text-4xl md:text-3xl font-bold p-2">Avg Rating: {avgRating}</h1>
           </li>
           <li className="p-4">
-            <h1 className="sm:text-sm lg:text-4xl md:text-3xl font-bold p-2">Budget</h1>
+            <h1 className="sm:text-sm lg:text-4xl md:text-3xl font-bold p-2">Budget: {budget}</h1>
           </li>
           <li className="p-4">
-            <h1 className="sm:text-sm lg:text-4xl md:text-3xl font-bold p-2">Meal of the Week</h1>
+            <h1 className="sm:text-sm lg:text-4xl md:text-3xl font-bold p-2">Meal of the Week: {meal_of_the_week}</h1>
           </li>
           <li className="p-4">
             <h1 className="sm:text-sm lg:text-4xl md:text-3xl font-bold p-2">Gallery</h1>
--- pages/restaurants/[slug].js
+++ pages/restaurants/[slug].js
@@ -22,7 +22,7 @@ export const getServerSideProps = async (context) => {
 };
 
 const RestaurantsPage = ({ restaurant, reviews }) => {
-  const { title, tags, images } = restaurant;
+  const { title, tags, images, avgPrice, rating } = restaurant;
   const currrentUser = {
     name: "John Doe",
     avatar: "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
@@ -41,7 +41,11 @@ const RestaurantsPage = ({ restaurant, reviews }) => {
         title={title}
         tags={tags}
       />
-      <CommuneInfoSection />
+      <CommuneInfoSection 
+      avgRating={rating}
+      budget={avgPrice}
+      meal_of_the_week={"SUSHI"}
+      />
       <div className="py-[3rem]">
         <h1 className="text-white text-5xl text-center">Reviews</h1>
       </div>
