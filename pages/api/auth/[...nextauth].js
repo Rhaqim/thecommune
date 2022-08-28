cf56b3f2489c03dc96cdcd9c7a617a0e104010be
Rhaqim <anusiemj@gmail.com>
2022-08-28 13:27:51 +0100

    added NextAuth for Authentication

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,19 @@
+import NextAuth  from 'next-auth/next';
+import GoogleProvider from 'next-auth/providers/google';
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import { connectToDatabase } from '../../../lib/DB/mongoDBConnect';
+
+export default NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.NEXT_GOOGLE_CLIENT_ID,
+            clientSecret: process.env.NEXT_GOOGLE_CLIENT_SECRET,
+            scope: ['profile', 'email']
+        })
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+    adapter: MongoDBAdapter({
+        client: connectToDatabase(),
+        collection: 'USERS'
+    }),
+})
