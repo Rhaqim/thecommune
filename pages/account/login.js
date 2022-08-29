e5baae52408b302a1856aefe4966402d46afcd0d
Rhaqim <anusiemj@gmail.com>
2022-08-29 21:06:29 +0100

    working on the login

--- pages/account/login.js
+++ pages/account/login.js
@@ -6,7 +6,7 @@ import { FaGoogle } from "react-icons/fa";
 
 const providers = [
     {
-        name: 'google',
+        Name: 'Google',
         Icon: FaGoogle,
     }
 ]
@@ -32,14 +32,22 @@ const Login = () => {
 
     return (
         <div className='max-w[1240px] justify-center mt-[5rem] items-center text-center py-4'>
-            {providers.map(({ name, Icon }) => (
-                <>
-                    <span className='ml-2'>{name}</span>
-                    <button key={name} className='font-bold bg-slate-400 p-2 rounded-lg px-4' onClick={() => signIn({ provider: name })}>
-                        <Icon />
-                    </button>
-                </>
-            ))}
+
+            <div className='flex flex-col items-center justify-center bg-white rounded-md mx-[22rem] h-40 text-black'>
+                <h1 className='text-3xl font-bold'>Login</h1>
+                <p className='text-xl'>Login to your account</p>
+
+                <div className='flex flex-col items-center justify-center mt-[2rem]'>
+                {providers.map(({ name, Icon }) => (
+                    <>
+                        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-[10rem] rounded-full' onClick={() => signIn(name)}>
+                            <Icon className='mx-2' size={25} />
+                            {name}
+                        </button>
+                    </>
+                ))}
+                </div>
+            </div>
         </div>
     )
 }
