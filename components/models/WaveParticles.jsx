8944714bae4ca4201ab36217a8c98ba2966a3b1a
Rhaqim <anusiemj@gmail.com>
2022-09-06 09:49:48 +0100

    changed perspective of the canvas camera - will work on particles soon.

--- components/models/WaveParticles.jsx
+++ components/models/WaveParticles.jsx
@@ -4,12 +4,12 @@ import { Canvas } from "@react-three/fiber";
 const WaveParticles = props => {
   return (
     <Canvas 
-    camera={{ position: [0, 7, 7] }}
+    camera={{ position: [0, -2, 3] }}
     {...props}>
       <ambientLight intensity={0.5} />
       <pointLight position={[10, 10, 10]} />
       <mesh>
-        <boxBufferGeometry attach="geometry" args={[20, 10, 0]} />
+        <boxBufferGeometry attach="geometry" args={[5, 3, 0]} />
         <meshStandardMaterial attach="material" color="white" />
       </mesh>
     </Canvas>
--- pages/events/index.js
+++ pages/events/index.js
@@ -3,31 +3,36 @@ import WaveParticles from '../../components/models/WaveParticles'
 
 const Events = () => {
     return (
-        <div className='relative mt-[5rem]'> 
-            <div className='scene'>
-                <WaveParticles className="bg-transparent" />
+        <div>
+            <div className='relative mt-[5rem] h-screen'>
+                <div className='scene'>
+                    <WaveParticles className="bg-transparent" />
+                </div>
+                <div>
+                    <h1 className='text-center text-3xl font-bold text-gray-800'>Events</h1>
+                    <p className='text-center text-gray-600'>
+                        Earn tokens everytime you attend an event.
+                    </p>
+                    <p className='text-center text-gray-600'>
+                        lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quae, quisquam.
+                    </p>
+                    <p className='text-center text-gray-600'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quae, quisquam.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quae, quisquam.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quae, quisquam.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quae, quisquam.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quae, quisquam.
+                    </p>
+                </div>
             </div>
             <div>
                 <h1 className='text-center text-3xl font-bold text-gray-800'>Events</h1>
-                <p className='text-center text-gray-600'>
-                    Earn tokens everytime you attend an event.
-                </p>
-                <p className='text-center text-gray-600'>
-                    lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quae, quisquam.
-                </p>
-                <p className='text-center text-gray-600'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quae, quisquam.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quae, quisquam.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quae, quisquam.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quae, quisquam.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quae, quisquam.
-                </p>
             </div>
         </div>
     )
