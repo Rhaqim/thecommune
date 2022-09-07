56483da148b8a4d58776b8143bfea474b94ed43a
Rhaqim <anusiemj@gmail.com>
2022-09-07 01:20:53 +0100

    added the scroll effect for scale

--- components/models/BackgroundScene.jsx
+++ components/models/BackgroundScene.jsx
@@ -1,5 +1,5 @@
-import React, { useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import React, { useState, useRef, useEffect } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
 
 const BackgroundScene = () => {
   return (
@@ -13,15 +13,35 @@ export default BackgroundScene;
 
 function Object(props) {
   const myRef = useRef();
+
+  const [active, setActive] = useState(false);
+
   useFrame(() => {
     if (myRef.current) {
       myRef.current.rotation.y += 0.01;
     }
   });
+
+  const handleScroll = () => {
+    if (window.scrollY >= 10) {
+      setActive(true);
+    } else {
+      setActive(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  },[]);
+
   return (
     <mesh 
     ref={myRef}
-    onPointerEnter={e => console.log(e)}
+    scale={active ? 1.5 : 1} 
+    onClick={() => setActive(!active)}
     position={[0, 0, 0]}
     >
       <boxBufferGeometry attach="geometry" args={[1, 2, 3]} />
