985bf16a7282e010e1247cc3a1653ffa41aa8456
Rhaqim <anusiemj@gmail.com>
2022-09-08 00:50:04 +0100

    added a second cube to test collisions

--- components/models/BackgroundScene.jsx
+++ components/models/BackgroundScene.jsx
@@ -13,34 +13,58 @@ export default BackgroundScene
 
 function Object(props) {
     const myRef = useRef()
+    const secRef = useRef()
 
     const [active, setActive] = useState(false)
 
-    useFrame(() => {
-        if (myRef.current) {
-            myRef.current.rotation.y += 0.01
-        }
-    })
+    // useFrame(() => {
+    //     if (myRef.current) {
+    //         myRef.current.rotation.y += 0.01
+    //     }
+    // })
 
-    const handleMouseMove = (e) => {
-        const { clientX, clientY } = e
-        // myRef.current.position.x = -(clientX / window.innerWidth) * 2 + 1;
-        // myRef.current.position.y = -(clientY / window.innerHeight) * 2 + 1;
-        myRef.current.position.x = clientX / window.innerWidth
-        myRef.current.position.y = -(clientY / window.innerHeight)
+    // const handleMouseMove = (e) => {
+    //     const { clientX, clientY } = e
+    //     // myRef.current.position.x = -(clientX / window.innerWidth) * 2 + 1;
+    //     // myRef.current.position.y = -(clientY / window.innerHeight) * 2 + 1;
+    //     myRef.current.position.x = clientX / window.innerWidth + 0.5
+    //     myRef.current.position.y = -(clientY / window.innerHeight)
+    // }
+
+    // const handleCollison = () => {
+    //     let meshBB = new THREE.Box3(
+    //         new THREE.Vector3(),
+    //         new THREE.Vector3()
+    //     ).setFromObject(myRef.current)
+    // }
+
+    const controls = (e) => {
+        if (e.keyCode === 37) {
+            myRef.current.position.x -= 1
+        } else if (e.keyCode === 39) {
+            myRef.current.position.x += 1
+        } else if (e.keyCode === 38) {
+            myRef.current.position.y += 1
+        } else if (e.keyCode === 40) {
+            myRef.current.position.y -= 1
+        }
     }
 
     useEffect(() => {
-        window.addEventListener('mousemove', handleMouseMove)
-        return () => {
-            window.removeEventListener('mousemove', handleMouseMove)
-        }
+        // window.addEventListener('mousemove', handleMouseMove)
+        window.addEventListener('keydown', controls)
     }, [])
 
     return (
-        <mesh ref={myRef} position={[0, 0, 0]}>
-            <boxBufferGeometry attach="geometry" args={[1, 2, 3]} />
-            <meshBasicMaterial attach="material" color="white" />
-        </mesh>
+        <>
+            <mesh ref={myRef} position={[-2, 0, 0]}>
+                <boxBufferGeometry attach="geometry" args={[1, 2, 3]} />
+                <meshBasicMaterial attach="material" color="white" />
+            </mesh>
+            <mesh ref={secRef} position={[2, 0, 0]}>
+                <boxBufferGeometry attach="geometry" args={[1, 2, 3]} />
+                <meshBasicMaterial attach="material" color="white" />
+            </mesh>
+        </>
     )
 }
